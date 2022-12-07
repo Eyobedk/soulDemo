1c2037364de7d5a21608e9732295a3cb4bfc38bf
Eyobedk <Eyobedkebede10@gmail.com>
2022-12-07 00:40:10 +0000

    changed route to root

--- src/api/research/router.js
+++ src/api/research/router.js
@@ -2,7 +2,7 @@ const Researches = require('./controller');
 const express = require("express");
 const router = express.Router();
 
-router.get('/results',Researches.getsingleResearch)
+router.get('/',Researches.getsingleResearch)
 
 router.get('/alls',Researches.getAllResearchs)
 
